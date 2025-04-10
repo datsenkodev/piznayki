d96ab8e1060940a3513c28056c52c997c1cf9ee1
Dmytro <datsenkodev@gmail.com>
2025-04-10 18:26:27 +0300

    Finish contacts page

--- src/app/contacts/page.jsx
+++ src/app/contacts/page.jsx
@@ -1,38 +1,153 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 import Image from 'next/image';
 import Link from 'next/link';
 
 import Kids from '@components/kids';
+import Map from '@contacts/map';
 
 import pin from 'public/images/icons/pin.svg';
+import phone from 'public/images/icons/phone-grey.svg';
+import mail from 'public/images/icons/mail-grey.svg';
 
 export default function Contacts() {
+  const [selectedMarker, setSelectedMarker] = useState('1');
+
+  const handleMarkerChange = (newKey) => {
+    setSelectedMarker(newKey);
+  };
+
   return (
-    <main>
-      <section className='map'>
-        map
+    <main className='contacts-page'>
+      <Map markerKey={selectedMarker} />
+      <div className='container pointer-events-none'>
+        <ul className='contacts-list'>
+          <li>
+            <label className='custom-radio' tabIndex={0} onClick={() => handleMarkerChange('1')}>
+              <input
+                type='radio'
+                id='first_location'
+                name='drone'
+                value='first_location'
+                defaultChecked
+              />
+              <span className='radio'></span>
+              <span className='text-[1rem] xs:text-xl font-bold'>Дитячий садок «Пізнайки»</span>
+              <span className='flex gap-1 text-xs md:text-[1rem] text-[#646464]'>
+                <Image src={pin} alt='Точка геолокації' />
+                Вул. Кургузова 1А, 3 корпус, офіс 569
+              </span>
+            </label>
+          </li>
+          <li>
+            <label className='custom-radio' tabIndex={0} onClick={() => handleMarkerChange('2')}>
+              <input type='radio' id='second_location' name='drone' value='second_location' />
+              <span className='radio'></span>
+              <span className='text-[1rem] xs:text-xl font-bold'>
+                Приватна початкова школа «Piznayki School» №1
+              </span>
+              <span className='flex gap-1 text-xs md:text-[1rem] text-[#646464]'>
+                <Image src={pin} alt='Точка геолокації' />
+                Вул.Симоненка, 4в (внутрішній двір)
+              </span>
+            </label>
+          </li>
+          <li>
+            <label className='custom-radio' tabIndex={0} onClick={() => handleMarkerChange('3')}>
+              <input type='radio' id='third_location' name='drone' value='third_location' />
+              <span className='radio'></span>
+              <span className='text-[1rem] xs:text-xl font-bold'>
+                Приватна початкова школа «Piznayki School» №2
+              </span>
+              <span className='flex gap-1 text-xs md:text-[1rem] text-[#646464]'>
+                <Image src={pin} alt='Точка геолокації' />
+                Проспект Шевченка, 6а
+              </span>
+            </label>
+          </li>
+          <li>
+            <label className='custom-radio' tabIndex={0} onClick={() => handleMarkerChange('4')}>
+              <input type='radio' id='fourth_location' name='drone' value='fourth_location' />
+              <span className='radio'></span>
+              <span className='text-[1rem] xs:text-xl font-bold'>
+                Репетиторський центр для 1-11 класів «Piznayki»
+              </span>
+              <span className='flex gap-1 text-xs md:text-[1rem] text-[#646464]'>
+                <Image src={pin} alt='Точка геолокації' />
+                Проспект Шевченка, 7а (0 поверх)
+              </span>
+            </label>
+          </li>
+          <li>
+            <label className='custom-radio' tabIndex={0} onClick={() => handleMarkerChange('5')}>
+              <input type='radio' id='fifth_location' name='drone' value='fifth_location' />
+              <span className='radio'></span>
+              <span className='text-[1rem] xs:text-xl font-bold'>Центр підготовки до школи</span>
+              <span className='flex gap-1 text-xs md:text-[1rem] text-[#646464]'>
+                <Image src={pin} alt='Точка геолокації' />
+                Проспект Шевченка 2д
+              </span>
+            </label>
+          </li>
+          <li>
+            <label className='custom-radio' tabIndex={0} onClick={() => handleMarkerChange('6')}>
+              <input type='radio' id='sixth_location' name='drone' value='sixth_location' />
+              <span className='radio'></span>
+              <span className='text-[1rem] xs:text-xl font-bold'>
+                Репетитор для учнів 1-4 класів
+              </span>
+              <span className='flex gap-1 text-xs md:text-[1rem] text-[#646464]'>
+                <Image src={pin} alt='Точка геолокації' />
+                Проспект Шевченка 6а
+              </span>
+            </label>
+          </li>
+          <li>
+            <label className='custom-radio' tabIndex={0} onClick={() => handleMarkerChange('7')}>
+              <input type='radio' id='seventh_location' name='drone' value='seventh_location' />
+              <span className='radio'></span>
+              <span className='text-[1rem] xs:text-xl font-bold'>
+                Приватна середня школа, Репетиторський центр для 5-11 класів
+              </span>
+              <span className='flex gap-1 text-xs md:text-[1rem] text-[#646464]'>
+                <Image src={pin} alt='Точка геолокації' />
+                Вулиця Шкільна 73а
+              </span>
+            </label>
+          </li>
+          <li>
+            <label className='custom-radio' tabIndex={0} onClick={() => handleMarkerChange('8')}>
+              <input type='radio' id='eight_location' name='drone' value='eight_location' />
+              <span className='radio'></span>
+              <span className='text-[1rem] xs:text-xl font-bold'>
+                Репетиторський центр для дітей з особливими освітніми потребами
+              </span>
+              <span className='flex gap-1 text-xs md:text-[1rem] text-[#646464]'>
+                <Image src={pin} alt='Точка геолокації' />
+                Проспект Шевченка 7а
+              </span>
+            </label>
+          </li>
+        </ul>
+      </div>
+      <section className='contacts-panel'>
         <div className='container'>
-          <ul className='bg-white p-7 md:px-10 md:py-11'>
-            <li>
-              <label>
-                <input
-                  type='radio'
-                  id='first_location'
-                  name='drone'
-                  value='first_location'
-                  checked
-                />
-                <span className='text-[1rem] xs:text-xl font-bold'>Дитячий садок «Пізнайки»n</span>
-                <span className='flex gap-1 text-xs md:text-[1rem] text-[#646464]'>
-                  <Image src={pin} />
-                  Вул. Кургузова 1А, 3 корпус, офіс 569
-                </span>
-              </label>
-            </li>
-          </ul>
+          <Link href={'tel:+380973383431'} className='contacts-panel__link'>
+            <Image src={phone} alt='зателефонувати' />
+            <div className='flex flex-col gap-1'>
+              <span>Наш номер телефону:</span>
+              <h3 className='underline-effect'>+38 097 338 34 31</h3>
+            </div>
+          </Link>
+          <Link href={'mailto:piznayko123@gmail.com'} className='contacts-panel__link'>
+            <Image src={mail} alt='написати на пошту' />
+            <div className='flex flex-col gap-1'>
+              <span>Наша пошта:</span>
+              <h3 className='underline-effect'>piznayko123@gmail.com</h3>
+            </div>
+          </Link>
         </div>
       </section>
       <Kids />
---
+++ src/pages/contacts/map.jsx
@@ -0,0 +1,129 @@
+'use client';
+
+import { useState, useEffect, useRef } from 'react';
+
+import { Wrapper } from '@googlemaps/react-wrapper';
+
+export default function Map({ markerKey }) {
+  return (
+    <section className='map'>
+      <Wrapper
+        apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}
+        version='beta'
+        libraries={['marker']}>
+        <MyMap markerKey={markerKey} />
+      </Wrapper>
+    </section>
+  );
+}
+
+function MyMap({ markerKey }) {
+  const [map, setMap] = useState(null);
+  const ref = useRef();
+
+  const mapOptions = {
+    mapId: process.env.NEXT_PUBLIC_MAP_ID,
+    center: markersArray.find((marker) => marker.key === markerKey)?.coordinates || {
+      lat: 50.583848,
+      lng: 30.474535,
+    },
+    zoom: 18,
+  };
+
+  useEffect(() => {
+    const mapInstance = new window.google.maps.Map(ref.current, mapOptions);
+    setMap(mapInstance);
+  }, []);
+
+  useEffect(() => {
+    if (map && markerKey) {
+      const newCenter = markersArray.find((marker) => marker.key === markerKey)?.coordinates;
+      if (newCenter) {
+        map.panTo(newCenter);
+      }
+    }
+  }, [map, markerKey]);
+
+  return (
+    <>
+      <div ref={ref} id='map' style={{ position: 'absolute' }} />
+      {Markers && <Markers map={map} />}
+    </>
+  );
+}
+
+const markersArray = [
+  {
+    key: '1',
+    coordinates: {
+      lat: 50.583848,
+      lng: 30.474535,
+    },
+  },
+  {
+    key: '2',
+    coordinates: {
+      lat: 50.58177980001313,
+      lng: 30.483049682555844,
+    },
+  },
+  {
+    key: '3',
+    coordinates: {
+      lat: 50.58086491651105,
+      lng: 30.490707567044723,
+    },
+  },
+  {
+    key: '4',
+    coordinates: {
+      lat: 50.582077724068434,
+      lng: 30.489622386820965,
+    },
+  },
+  {
+    key: '5',
+    coordinates: {
+      lat: 50.581024012433616,
+      lng: 30.48932290892126,
+    },
+  },
+  {
+    key: '6',
+    coordinates: {
+      lat: 50.58086491651105,
+      lng: 30.490707567044723,
+    },
+  },
+  {
+    key: '7',
+    coordinates: {
+      lat: 50.58291163246434,
+      lng: 30.48712822665436,
+    },
+  },
+  {
+    key: '8',
+    coordinates: {
+      lat: 50.582112768494895,
+      lng: 30.489649756091083,
+    },
+  },
+];
+
+function Markers({ map }) {
+  const [data, setData] = useState(markersArray);
+
+  useEffect(() => {
+    if (!map) return;
+
+    data.forEach((marker) => {
+      const markerInstance = new google.maps.Marker({
+        position: marker.coordinates,
+        map: map,
+      });
+    });
+  }, [map, data]);
+
+  return null;
+}
